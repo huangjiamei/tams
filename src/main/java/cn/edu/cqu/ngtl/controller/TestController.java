462e7edb16aa17a1cdf6398c0a8408fbc4a50fa0
a343902152 <260174969@qq.com>
2016-10-16 12:29:02 +0800

    添加任务详情页面，重构box-header模板

--- src/main/java/cn/edu/cqu/ngtl/controller/TestController.java
+++ src/main/java/cn/edu/cqu/ngtl/controller/TestController.java
@@ -19,23 +19,23 @@ import java.util.List;
  */
 @Controller
 @RequestMapping("/mytest")
-public class TestController extends UifControllerBase{
+public class TestController extends UifControllerBase {
 
 
     @RequestMapping("/func")
-    public void func(){
+    public void func() {
         System.out.println("func");
     }
 
     @RequestMapping(params = "methodToCall=getMyInformationPage")
-    public ModelAndView getMyInformationPage(@ModelAttribute("KualiForm") UifFormBase form){
-        TestForm testForm=(TestForm) form;
+    public ModelAndView getMyInformationPage(@ModelAttribute("KualiForm") UifFormBase form) {
+        TestForm testForm = (TestForm) form;
         return this.getModelAndView(testForm, "pageTest1");
     }
 
     @RequestMapping(params = "methodToCall=getDatatablePage")
-    public ModelAndView getDatatablePage(@ModelAttribute("KualiForm") UifFormBase form){
-        TestForm testForm=(TestForm) form;
+    public ModelAndView getDatatablePage(@ModelAttribute("KualiForm") UifFormBase form) {
+        TestForm testForm = (TestForm) form;
         return this.getModelAndView(testForm, "pageDatatable");
     }
 
@@ -43,60 +43,67 @@ public class TestController extends UifControllerBase{
     // ------------ pages原型 --------------
 
     @RequestMapping(params = "methodToCall=getTeacherCoursePage")
-    public ModelAndView getTeacherCoursePage(@ModelAttribute("KualiForm") UifFormBase form){
-        TestForm testForm=(TestForm) form;
+    public ModelAndView getTeacherCoursePage(@ModelAttribute("KualiForm") UifFormBase form) {
+        TestForm testForm = (TestForm) form;
 
         return this.getModelAndView(testForm, "pageCourseTeacher");
     }
 
     @RequestMapping(params = "methodToCall=getApplyTAPage")
-    public ModelAndView getApplyTAPage(@ModelAttribute("KualiForm") UifFormBase form){
-        TestForm testForm=(TestForm) form;
+    public ModelAndView getApplyTAPage(@ModelAttribute("KualiForm") UifFormBase form) {
+        TestForm testForm = (TestForm) form;
         return this.getModelAndView(testForm, "pageApplyForTaForm");
     }
 
     @RequestMapping(params = "methodToCall=getCommonHome")
-    public ModelAndView getCommonHome(@ModelAttribute("KualiForm") UifFormBase form){
-        TestForm testForm=(TestForm) form;
+    public ModelAndView getCommonHome(@ModelAttribute("KualiForm") UifFormBase form) {
+        TestForm testForm = (TestForm) form;
         return this.getModelAndView(testForm, "pageCommonHome");
     }
 
     @RequestMapping(params = "methodToCall=getTaskListPage")
-    public ModelAndView getTaskListPage(@ModelAttribute("KualiForm") UifFormBase form){
-        TestForm testForm=(TestForm) form;
+    public ModelAndView getTaskListPage(@ModelAttribute("KualiForm") UifFormBase form) {
+        TestForm testForm = (TestForm) form;
         return this.getModelAndView(testForm, "pageTaskList");
     }
 
     @RequestMapping(params = "methodToCall=getClassInfo")
-    public ModelAndView getClassInfo(@ModelAttribute("KualiForm") UifFormBase form){
-        TestForm testForm=(TestForm) form;
+    public ModelAndView getClassInfo(@ModelAttribute("KualiForm") UifFormBase form) {
+        TestForm testForm = (TestForm) form;
         return this.getModelAndView(testForm, "pageClassInfo");
     }
 
-    // ------------ 部分后台调用 --------------
-    @RequestMapping(params = {"pageId=pageCourseTeacher","methodToCall=getTaskListDetail" })
-    public ModelAndView getTaskListDetail(@ModelAttribute("KualiForm") UifFormBase form, HttpServletRequest request,
-                                        HttpServletResponse response) throws Exception {
-        TestForm testForm=(TestForm) form;
+    @RequestMapping(params = "methodToCall=getTaskDetail")
+    public ModelAndView getTaskDetail(@ModelAttribute("KualiForm") UifFormBase form) {
+        TestForm testForm = (TestForm) form;
+        return this.getModelAndView(testForm, "pageTaskDetail");
+    }
 
-        try{
-            CollectionControllerServiceImpl.CollectionActionParameters params = new CollectionControllerServiceImpl.CollectionActionParameters(testForm, true);
+    // ------------ 部分后台调用 --------------
+    @RequestMapping(params = {"pageId=pageCourseTeacher", "methodToCall=getTaskListDetail"})
+    public ModelAndView getTaskListDetail(@ModelAttribute("KualiForm") UifFormBase form, HttpServletRequest request,
+                                          HttpServletResponse response) throws Exception {
+        TestForm testForm = (TestForm) form;
+
+        try {
+            CollectionControllerServiceImpl.CollectionActionParameters params =
+                    new CollectionControllerServiceImpl.CollectionActionParameters(testForm, true);
             int index = params.getSelectedLineIndex();
 
-            TestObject object= testForm.getCollection().get(index);
-        }catch (Exception e){
+            TestObject object = testForm.getCollection().get(index);
+        } catch (Exception e) {
 
         }
 
         // FIXME: 2016/10/15 可以触发successCallback，但是btn的navigatetoPage出错
         // FIXME: 2016/10/15 即使换成btn也会出错？？普通的申请表用btn提交不会有这种问题，可能与datatable的特性有关？
-        return this.getModelAndView(testForm, "pageCommonHome");
+        return this.getModelAndView(testForm, "pageTaskList");
     }
 
-    @RequestMapping(params = {"pageId=pageApplyForTaForm","methodToCall=submitTaForm" })
+    @RequestMapping(params = {"pageId=pageApplyForTaForm", "methodToCall=submitTaForm"})
     public ModelAndView submitTaForm(@ModelAttribute("KualiForm") UifFormBase form, HttpServletRequest request,
-                                          HttpServletResponse response) throws Exception {
-        TestForm testForm=(TestForm) form;
+                                     HttpServletResponse response) throws Exception {
+        TestForm testForm = (TestForm) form;
 
         return this.getModelAndView(testForm, "pageCommonHome");
     }

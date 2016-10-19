00b70dfb7efacdf3e70aefeda38fada76049c569
lxsgdsgg <1021357465@qq.com>
2016-10-19 10:46:02 +0800

    初步解决页面性能问题

---
+++ src/main/java/cn/edu/cqu/ngtl/dao/ut/impl/UTClassInfoDaoJpa.java
@@ -0,0 +1,34 @@
+package cn.edu.cqu.ngtl.dao.ut.impl;
+
+import cn.edu.cqu.ngtl.dao.ut.UTClassInfoDao;
+import cn.edu.cqu.ngtl.dataobject.view.UTClassInformation;
+import org.kuali.rice.krad.service.KRADServiceLocator;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import java.util.List;
+
+/**
+ * Created by awake on 2016-10-19.
+ */
+@Repository
+@Component("UTClassInfoDaoJpa")
+public class UTClassInfoDaoJpa implements UTClassInfoDao {
+
+    EntityManager em =  KRADServiceLocator.getEntityManagerFactory().createEntityManager();
+
+
+    @Override
+    public List<UTClassInformation> getAllClassInformation(){
+        Query query = em.createNativeQuery("SELECT * FROM UNITIME_CLASS_INFORMATION",UTClassInformation.class);
+        List<UTClassInformation> result = query.getResultList();
+        return result;
+    }
+
+
+
+
+
+}

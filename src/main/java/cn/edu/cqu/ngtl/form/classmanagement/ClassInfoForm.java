73e20edaa184835fe0904e3f5f55c1b539b668d6
cqEguil <artisan6g@outlook.com>
2016-10-23 09:43:53 +0800

    统一页面布局框架和样式，除去exam.css

--- src/main/java/cn/edu/cqu/ngtl/form/classmanagement/ClassInfoForm.java
+++ src/main/java/cn/edu/cqu/ngtl/form/classmanagement/ClassInfoForm.java
@@ -13,7 +13,136 @@ import java.util.List;
 public class ClassInfoForm extends BaseForm implements Serializable {
 
 
+
+
     private static final long serialVersionUID = 230347454225549981L;
+
+
+
+    /*
+    班级管理隐藏搜索框
+     */
+    String condDepartmentName;
+    String condCourseName;
+    String condCourseCode;
+    String condClassNumber;
+    String condInstructorName;
+
+    String condJudgeStatus;
+    String condCourseHour;
+    String condCourseCredit;
+    String condIsRequired;
+    String condCourseClassification;
+    String condSessionYear;
+    String condProgramName;
+
+
+    public void setCondDepartmentName(String condDepartmentName) {
+        this.condDepartmentName = condDepartmentName;
+    }
+
+    public void setCondCourseName(String condCourseName) {
+        this.condCourseName = condCourseName;
+    }
+
+    public void setCondCourseCode(String condCourseCode) {
+        this.condCourseCode = condCourseCode;
+    }
+
+    public void setCondClassNumber(String condClassNumber) {
+        this.condClassNumber = condClassNumber;
+    }
+
+    public void setCondInstructorName(String condInstructorName) {
+        this.condInstructorName = condInstructorName;
+    }
+
+
+
+    public void setCondJudgeStatus(String condJudgeStatus) {
+        this.condJudgeStatus = condJudgeStatus;
+    }
+
+    public void setCondCourseHour(String condCourseHour) {
+        this.condCourseHour = condCourseHour;
+    }
+
+    public void setCondCourseCredit(String condCourseCredit) {
+        this.condCourseCredit = condCourseCredit;
+    }
+
+    public void setCondIsRequired(String condIsRequired) {
+        this.condIsRequired = condIsRequired;
+    }
+
+    public void setCondCourseClassification(String condCourseClassification) {
+        this.condCourseClassification = condCourseClassification;
+    }
+
+    public void setCondSessionYear(String condSessionYear) {
+        this.condSessionYear = condSessionYear;
+    }
+
+    public void setCondProgramName(String condProgramName) {
+        this.condProgramName = condProgramName;
+    }
+
+
+
+    public static long getSerialVersionUID() {
+
+        return serialVersionUID;
+    }
+
+    public String getCondDepartmentName() {
+        return condDepartmentName;
+    }
+
+    public String getCondCourseName() {
+        return condCourseName;
+    }
+
+    public String getCondCourseCode() {
+        return condCourseCode;
+    }
+
+    public String getCondClassNumber() {
+        return condClassNumber;
+    }
+
+    public String getCondInstructorName() {
+        return condInstructorName;
+    }
+
+
+    public String getCondJudgeStatus() {
+        return condJudgeStatus;
+    }
+
+    public String getCondCourseHour() {
+        return condCourseHour;
+    }
+
+    public String getCondCourseCredit() {
+        return condCourseCredit;
+    }
+
+    public String getCondIsRequired() {
+        return condIsRequired;
+    }
+
+    public String getCondCourseClassification() {
+        return condCourseClassification;
+    }
+
+    public String getCondSessionYear() {
+        return condSessionYear;
+    }
+
+    public String getCondProgramName() {
+        return condProgramName;
+    }
+
 
     private List<ClassTeacherViewObject> collection = new ArrayList<>();
 

63de2df7f53dfcc5f137d5ccc627104adc492bd7
lxsgdsgg <1021357465@qq.com>
2016-10-26 17:50:29 +0800

    完善课程负责人页面

---
+++ src/main/java/cn/edu/cqu/ngtl/viewobject/adminInfo/CourseManagerViewObject.java
@@ -0,0 +1,53 @@
+package cn.edu.cqu.ngtl.viewobject.adminInfo;
+
+/**
+ * Created by awake on 2016-10-26.
+ */
+public class CourseManagerViewObject {
+
+    private String id ;
+    private String courseNm;
+    private String courseNmb;
+    private String courseManager;
+    private String InstructorCode;
+
+    public String getCourseNm() {
+        return courseNm;
+    }
+
+    public void setCourseNm(String courseNm) {
+        this.courseNm = courseNm;
+    }
+
+    public String getCourseNmb() {
+        return courseNmb;
+    }
+
+    public void setCourseNmb(String courseNmb) {
+        this.courseNmb = courseNmb;
+    }
+
+    public String getCourseManager() {
+        return courseManager;
+    }
+
+    public void setCourseManager(String courseManager) {
+        this.courseManager = courseManager;
+    }
+
+    public String getInstructorCode() {
+        return InstructorCode;
+    }
+
+    public void setInstructorCode(String instructorCode) {
+        InstructorCode = instructorCode;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}

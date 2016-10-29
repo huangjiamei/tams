1d714e527ef17aa38229027e632dcac5f1cf6b86
cqEguil <artisan6g@outlook.com>
2016-10-29 15:46:30 +0800

    优化导航栏，增强移动端适应性

--- src/main/webapp/js/tams.js
+++ src/main/webapp/js/tams.js
@@ -12,7 +12,9 @@ function doEditorPreSubmit(editorID,transferID) {
 
 //初始化导航栏对话框
 function initNavDialog() {
-    //alert(jQuery(".nav-pills a").size());
+    //隐藏uif-dialog自带的header
+    jQuery(".navigationDialog>.modal-content>.modal-header").hide();
+
     //设置nav-pills中各个选项的点击事件为切换导航栏目
     jQuery(".nav-pills a").click(function(e){
         //将当前导航栏目隐藏
@@ -20,6 +22,13 @@ function initNavDialog() {
         //显示选择的导航栏目
         jQuery(this).tab('show');
     });
+
+    //关闭点击事件
+    jQuery(".navigationDialog .close").click(function(a) {
+        a.stopPropagation();//让点击事件停止传播以免关掉整个dialog
+        //去除选中效果
+        jQuery(".navigationDialog .nav-pills .active").removeClass("active"), jQuery(".navigationDialog .tab-content .active").removeClass("active")
+    })
 }
 
 

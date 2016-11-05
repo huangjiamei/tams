85d627ee6b91db415c8bf0501ceb9d9d5f7d9a7c
tangjing <tangjing1996@gmail.com>
2016-11-05 11:56:25 +0800

    refs #851 下拉框联动

--- src/main/java/cn/edu/cqu/ngtl/dao/cm/CMProgramDao.java
+++ src/main/java/cn/edu/cqu/ngtl/dao/cm/CMProgramDao.java
@@ -2,6 +2,8 @@ package cn.edu.cqu.ngtl.dao.cm;
 
 import cn.edu.cqu.ngtl.dataobject.cm.CMProgram;
 
+import java.util.List;
+
 /**
  * Created by 金祖增 on 2016/10/21.
  */
@@ -9,4 +11,5 @@ public interface CMProgramDao {
 
     CMProgram getCMProgramById(Integer id);
 
+    List<CMProgram> selectByDepartmentId(String condDepartmentName);
 }

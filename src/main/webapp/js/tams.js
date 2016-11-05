e79baabf57036b7bf0a7c65e2a274f22d63eb224
cqEguil <artisan6g@outlook.com>
2016-11-05 10:48:00 +0800

    导航栏打开默认切换到第一项

--- src/main/webapp/js/tams.js
+++ src/main/webapp/js/tams.js
@@ -10,25 +10,24 @@ function doEditorPreSubmit(editorID,transferID) {
 }
 
 
-//��ʼ���������Ի���
+
 function initNavDialog() {
-    //ȥ��uif-dialog�Դ���header
+    jQuery(".navigationDialog .nav>ul>:first-child>a").tab('show');
+    //去掉dialog本身的header
     jQuery(".navigationDialog>.modal-content>.modal-header").remove();
 
     jQuery("#navDialog").removeClass("fade");
-    //����nav-pills�и���ѡ��ĵ���¼�Ϊ�л�������Ŀ
+    //添加导航栏点击切换事件
     jQuery(".navigationDialog .nav a").click(function(e){
-        //����ǰ������Ŀ����
         e.preventDefault();
-        //��ʾѡ��ĵ�����Ŀ
         jQuery(this).tab('show');
     });
 
-    //�رյ���¼�
+
     jQuery(".navigationDialog .close").click(function(a) {
-        a.stopPropagation();//�õ���¼�ֹͣ��������ص�����dialog
-        //ȥ��ѡ��Ч��
-        jQuery(".navigationDialog .nav .active").removeClass("active"), jQuery(".navigationDialog .tab-content .active").removeClass("active")
+        a.stopPropagation();
+        jQuery(".navigationDialog .nav .active").removeClass("active");
+        jQuery(".navigationDialog .tab-content .active").removeClass("active");
     })
 }
 

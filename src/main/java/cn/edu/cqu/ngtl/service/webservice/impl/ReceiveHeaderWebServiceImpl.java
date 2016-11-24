31fb211b8815e75efa33fb9253b851e487957db5
lxsgdsgg <1021357465@qq.com>
2016-11-24 10:16:06 +0800

    新增webservice调用权限

---
+++ src/main/java/cn/edu/cqu/ngtl/service/webservice/impl/ReceiveHeaderWebServiceImpl.java
@@ -0,0 +1,32 @@
+package cn.edu.cqu.ngtl.service.webservice.impl;
+
+import cn.edu.cqu.ngtl.service.webservice.ReceiveHeaderWebService;
+import org.apache.cxf.interceptor.LoggingInInterceptor;
+import org.apache.cxf.interceptor.LoggingOutInterceptor;
+import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
+
+/**
+ * Created by awake on 2016/11/24.
+ */
+public class ReceiveHeaderWebServiceImpl implements ReceiveHeaderWebService {
+
+    @Override
+    public boolean getResultFromHeaderWebService(String principalId,String permName) throws Exception {
+        Boolean bool = false;
+        ClassLoader originalClassLoader = Thread.currentThread().getContextClassLoader();
+        try {
+            JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
+            org.apache.cxf.endpoint.Client client = dcf.createClient("http://isse.cqu.edu.cn/exam/remoting/HeaderWebService?wsdl");
+            client.getInInterceptors().add(new LoggingInInterceptor());
+            client.getOutInterceptors().add(new LoggingOutInterceptor());
+            Object[] objects = client.invoke("hasPermission",principalId,permName);
+            bool = Boolean.parseBoolean(objects[0].toString());
+        }
+        finally {
+            Thread.currentThread().setContextClassLoader(originalClassLoader);
+        }
+        return bool;
+    }
+
+
+}

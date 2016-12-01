343c4137a0399d8dc4106e32b11221a6e38e0cdf
lxsgdsgg <1021357465@qq.com>
2016-12-01 17:04:34 +0800

    在basestart中添加当前学期信息

--- src/main/java/cn/edu/cqu/ngtl/controller/BaseController.java
+++ src/main/java/cn/edu/cqu/ngtl/controller/BaseController.java
@@ -1,11 +1,14 @@
 package cn.edu.cqu.ngtl.controller;
 
 import cn.edu.cqu.ngtl.bo.User;
+import cn.edu.cqu.ngtl.dao.ut.UTSessionDao;
+import cn.edu.cqu.ngtl.dataobject.ut.UTSession;
 import cn.edu.cqu.ngtl.form.BaseForm;
 import cn.edu.cqu.ngtl.form.TestForm;
 import org.kuali.rice.krad.util.GlobalVariables;
 import org.kuali.rice.krad.web.controller.UifControllerBase;
 import org.kuali.rice.krad.web.form.UifFormBase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -25,10 +28,16 @@ public class BaseController extends UifControllerBase {
      */
 
 
+    @Autowired
+    private UTSessionDao sessionDao;
+
+
     public ModelAndView baseStart(BaseForm form) {
         User user = (User) GlobalVariables.getUserSession().retrieveObject("user");
         form.setUser(user);
         String code = user.getCode();
+        UTSession curSession = sessionDao.getCurrentSession();
+        form.setCurrenSessionId(curSession.getId());
         return super.start(form);
     }
 

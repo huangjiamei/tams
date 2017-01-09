1b17822887f157546f0c29e38662ea63afff6181
lxsgdsgg <1021357465@qq.com>
2017-01-10 06:55:58 +0800

    增加周次 step5

--- src/main/java/cn/edu/cqu/ngtl/optionfinder/classview/ClassTeachWeekOptionFinder.java
+++ src/main/java/cn/edu/cqu/ngtl/optionfinder/classview/ClassTeachWeekOptionFinder.java
@@ -38,33 +38,33 @@ public class ClassTeachWeekOptionFinder extends UifKeyValuesFinderBase {
             }
         }*/
 
-        keyValues.add(new ConcreteKeyValue("第-2周(开学前2周)", "第-2周(开学前1周)"));
+        keyValues.add(new ConcreteKeyValue("第-2周(开学前2周)", "第-2周(开学前2周)"));
         keyValues.add(new ConcreteKeyValue("第-1周(开学前1周)", "第-1周(开学前1周)"));
-        keyValues.add(new ConcreteKeyValue("第1周", "第1周)"));
-        keyValues.add(new ConcreteKeyValue("第2周", "第2周)"));
-        keyValues.add(new ConcreteKeyValue("第3周", "第3周)"));
-        keyValues.add(new ConcreteKeyValue("第4周", "第4周)"));
-        keyValues.add(new ConcreteKeyValue("第5周", "第5周)"));
-        keyValues.add(new ConcreteKeyValue("第6周", "第6周)"));
-        keyValues.add(new ConcreteKeyValue("第7周", "第7周)"));
-        keyValues.add(new ConcreteKeyValue("第8周", "第8周)"));
-        keyValues.add(new ConcreteKeyValue("第9周", "第9周)"));
-        keyValues.add(new ConcreteKeyValue("第10周", "第10周)"));
-        keyValues.add(new ConcreteKeyValue("第11周", "第11周)"));
-        keyValues.add(new ConcreteKeyValue("第12周", "第12周)"));
-        keyValues.add(new ConcreteKeyValue("第13周", "第13周)"));
-        keyValues.add(new ConcreteKeyValue("第14周", "第14周)"));
-        keyValues.add(new ConcreteKeyValue("第15周", "第15周)"));
-        keyValues.add(new ConcreteKeyValue("第16周", "第16周)"));
-        keyValues.add(new ConcreteKeyValue("第17周", "第17周)"));
-        keyValues.add(new ConcreteKeyValue("第18周", "第18周)"));
-        keyValues.add(new ConcreteKeyValue("第19周", "第19周)"));
-        keyValues.add(new ConcreteKeyValue("第20周", "第20周)"));
-        keyValues.add(new ConcreteKeyValue("第21周", "第21周)"));
-        keyValues.add(new ConcreteKeyValue("第22周", "第22周)"));
-        keyValues.add(new ConcreteKeyValue("第23周", "第23周)"));
-        keyValues.add(new ConcreteKeyValue("第24周", "第24周)"));
-        keyValues.add(new ConcreteKeyValue("第25周", "第25周)"));
+        keyValues.add(new ConcreteKeyValue("第1周", "第1周"));
+        keyValues.add(new ConcreteKeyValue("第2周", "第2周"));
+        keyValues.add(new ConcreteKeyValue("第3周", "第3周"));
+        keyValues.add(new ConcreteKeyValue("第4周", "第4周"));
+        keyValues.add(new ConcreteKeyValue("第5周", "第5周"));
+        keyValues.add(new ConcreteKeyValue("第6周", "第6周"));
+        keyValues.add(new ConcreteKeyValue("第7周", "第7周"));
+        keyValues.add(new ConcreteKeyValue("第8周", "第8周"));
+        keyValues.add(new ConcreteKeyValue("第9周", "第9周"));
+        keyValues.add(new ConcreteKeyValue("第10周", "第10"));
+        keyValues.add(new ConcreteKeyValue("第11周", "第11周"));
+        keyValues.add(new ConcreteKeyValue("第12周", "第12周"));
+        keyValues.add(new ConcreteKeyValue("第13周", "第13周"));
+        keyValues.add(new ConcreteKeyValue("第14周", "第14周"));
+        keyValues.add(new ConcreteKeyValue("第15周", "第15周"));
+        keyValues.add(new ConcreteKeyValue("第16周", "第16周"));
+        keyValues.add(new ConcreteKeyValue("第17周", "第17周"));
+        keyValues.add(new ConcreteKeyValue("第18周", "第18周"));
+        keyValues.add(new ConcreteKeyValue("第19周", "第19周"));
+        keyValues.add(new ConcreteKeyValue("第20周", "第20周"));
+        keyValues.add(new ConcreteKeyValue("第21周", "第21周"));
+        keyValues.add(new ConcreteKeyValue("第22周", "第22周"));
+        keyValues.add(new ConcreteKeyValue("第23周", "第23周"));
+        keyValues.add(new ConcreteKeyValue("第24周", "第24周"));
+        keyValues.add(new ConcreteKeyValue("第25周", "第25周"));
 
 
 
